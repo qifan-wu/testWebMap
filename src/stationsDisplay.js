9a4daff59edb313ad5fd478b95bdb8cc581d8671
qifan-wu <qifanw@umich.edu>
2024-02-14 02:31:34 -0500

    display stations

---
+++ src/stationsDisplay.js
@@ -0,0 +1,32 @@
+import { map, stationsDataFile } from './universal.js'
+import { searchPOI } from './searchPOI.js';
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+function createStationMarkers(stationsData) {
+    var markers = L.markerClusterGroup();
+    stationsData.forEach(function (station) {
+        var marker = L.marker(new L.LatLng(station.lat, station.lon));
+        // marker.bindPopup(document.createTextNode(city.n_station).textContent);
+        marker.on('click', function() {
+            searchPOI(station.lat, station.lon);
+        });
+
+        markers.addLayer(marker);
+    });
+    map.addLayer(markers);
+}
+
+axios.get(stationsDataFile)
+    .then(function (response) {
+        var stationsData = response.data;
+        console.log(new Date().toLocaleString());
+        createStationMarkers(stationsData);
+        console.log(new Date().toLocaleString());
+    })
+    .catch(function (error) {
+        console.error("error fetching cache: ", error)
+    });
+
