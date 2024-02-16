da7b260495a003a7ff26579c3866695a861c7aae
qifan-wu <qifanw@umich.edu>
2024-02-16 05:10:35 -0500

    display poi in cluster group

--- src/searchPOI.js
+++ src/searchPOI.js
@@ -17,30 +17,30 @@ export function searchPOI(lat, lon) {
         out qt;`;
 
     // map.setView(new L.LatLng(lat, lon), 14);
-    map.setView(new L.LatLng(40.7128, -74.0060), 14);
+    map.setView(new L.LatLng(lat, lon), 14);
     var opl = new L.OverPassLayer({
         minZoom: 14,
         'query': poiQuery,
 
-        // onSuccess: function(data) {
-        // var poiGroup= L.markerClusterGroup({ showCoverageOnHover: true, disableClusteringAtZoom:18 });
-        // for (var i=0; i<data.elements.length; i++) {
-        //     var e = data.elements[i];
-        //     var pos = new L.LatLng(e.lat, e.lon);
-        //     // console.info(e.tags);
-        //     L.marker(pos,{
-        //         // icon:restaurantIcon,
-        //         title:e.tags.name, //shows restaurants names
-        //         tipus:e.tags.amenity
-        //     }).on('click', markerOnClick).addTo(poiGroup); //add markers to the cluster
-        // }
-        // map.addLayer(poiGroup); //to add the cluster to the map
+        onSuccess: function(data) {
+        var poiGroup= L.markerClusterGroup({ showCoverageOnHover: true, disableClusteringAtZoom:18 });
+        for (var i=0; i<data.elements.length; i++) {
+            var e = data.elements[i];
+            var pos = new L.LatLng(e.lat, e.lon);
+            // console.info(e.tags);
+            L.marker(pos,{
+                // icon:restaurantIcon,
+                title:e.tags.name, //shows restaurants names
+                tipus:e.tags.amenity
+            }).on('click', markerOnClick).addTo(poiGroup); //add markers to the cluster
+        }
+        map.addLayer(poiGroup); //to add the cluster to the map
 
-        // function markerOnClick(event){
-        //     var restaurante = event.target.options.tipus + " " +event.target.options.title;
-        //     event.target.bindPopup(restaurante).openPopup();
-        // }
-        // },
+        function markerOnClick(event){
+            var restaurante = event.target.options.tipus + " " +event.target.options.title;
+            event.target.bindPopup(restaurante).openPopup();
+        }
+        },
     });
     map.addLayer(opl);
     console.log('zzzzz');
