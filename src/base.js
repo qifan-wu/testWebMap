bd702fcdad9d2e284274dae13ab0c17c8fe96fbc
qifan-wu <qifanw@umich.edu>
2024-02-16 13:44:31 -0500

    move basemap to base.js

--- src/base.js
+++ src/base.js
@@ -1,6 +1,10 @@
 import { stationsDataFile } from './constants.js'
 import { createStationMarkers} from './utils.js';
 
+var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        })
+map.addLayer(basemap);
 
 axios.get(stationsDataFile)
     .then(function (response) {
