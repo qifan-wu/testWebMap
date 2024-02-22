80a95073f26449d03cb0d99526ec5dd77d5ea60c
qifan-wu <qifanw@umich.edu>
2024-02-21 19:07:28 -0500

    poi icon

--- src/icons.js
+++ src/icons.js
@@ -2,3 +2,11 @@ export var subwayIcon = L.icon({
     iconUrl: 'static/images/subway.svg',
     iconSize: [25, 25]
 });
+
+
+
+export var poiIcon = L.icon({
+    iconUrl: 'static/images/subway.svg',
+    iconSize: [10, 10]
+});
+
