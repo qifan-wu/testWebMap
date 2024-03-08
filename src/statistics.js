d1ce9753e1528b9660a0264c179dd998e5029d8a
qifan-wu <qifanw@umich.edu>
2024-03-08 03:19:30 -0500

    handle api async

--- src/statistics.js
+++ src/statistics.js
@@ -4,11 +4,13 @@ export async function displayStatistics(lat, lon) {
     // debugger;
     var buildingArea = await getBuildingArea(lat, lon);
 
-    var roadInfo = getRoadLength(lat, lon);
-    console.log("--------");
-    console.log(buildingArea);
+    var highwayStatistics = await getRoadLength(lat, lon);
+
     const buildingDiv = document.getElementById("buildingAreaInfo");
     buildingDiv.innerHTML = "Total Building Area: " + buildingArea + " square meter";
+
+    const highwayDiv = document.getElementById('highwayInfo');
+    highwayDiv.innerHTML = highwayStatistics;
 }
 
 export async function getBuildingArea(lat, lon) {
@@ -28,34 +30,18 @@ export async function getBuildingArea(lat, lon) {
         }
         const data = await response.json();
         var buildingArea = data.result[0].value;
-        console.log(buildingArea);
+
         return buildingArea;
     } catch (error) {
-        console.error("Error:", error);
+        console.error("Error fetching building area:", error);
         return -1;
     }
 
-        // .then(response => {
-        //     if (!response.ok) {
-        //         throw new Error(`Failed to fetch statistics: ${response.status}`);
-        //     }
-        //     return response.json();
-        // })
-        // .then(data => {
-        //     // console.log(data); // Process the response data as needed
-        //     var buildingArea = data.result[0].value;
-        //     console.log(buildingArea);
-        //     return buildingArea;
-        //     // resultDiv.innerHTML = "Total Building Area: " + buildingArea + " square meter";
-        // })
-        // .catch(error => {
-        //     console.error("Error:", error);
-        //     return -1;
-        // });
+
 };
 
 
-export function getRoadLength(lat, lon) {
+export async function getRoadLength(lat, lon) {
 
     let bcircle = [lon,lat, SEARCH_RADIUS_METER]
     const params = new URLSearchParams({
@@ -67,37 +53,27 @@ export function getRoadLength(lat, lon) {
     });
 
     const url = `${OHSOME_ENDPOINT}/length/groupBy/tag?${params}`;
-    const highwayDiv = document.getElementById('highwayInfo');
+    // const highwayDiv = document.getElementById('highwayInfo');
 
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Failed to fetch statistics: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            // console.log(data); // Process the response data as needed
-            // console.log(data.groupByResult);
-            var highwayStatistics = "";
-            data.groupByResult.forEach((highway) => {
-                // console.log(highway);
-                // console.log(highway.groupByObject);
-                // console.log(highway.result[0].value);
-                const highwayInfo = `Type ${highway.groupByObject} has total length: ${highway.result[0].value}m`;
-                highwayStatistics += `<p>${highwayInfo}</p>`;
-                // highwayStatistics += '<p>vlaaaa</p>';
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch road length statistics: ${response.status}`);
+        }
+        const data = await response.json();
 
-
-            });
-            highwayDiv.innerHTML = highwayStatistics;
-            // for (var obj in data.groupByResult) {
-            //     console.log(obj);
-            //     // console.log(obj.groupByObject);
-            //     // console.log(obj.result);
-            // }
-        })
-        .catch(error => {
-            console.error("Error:", error);
+        var totalRoadLen = 0;
+        var highwayStatistics = "";
+        data.groupByResult.forEach((highway) => {
+            const roadLen = highway.result[0].value;
+            const highwayInfo = `Type "${highway.groupByObject}" has length: ${roadLen}m`;
+            highwayStatistics += `<p>${highwayInfo}</p>`;
+            totalRoadLen += roadLen;
         });
+        return `<p>Total Road Length is ${totalRoadLen}m</p>` + highwayStatistics;
+
+    } catch (error) {
+        console.error("Error fetching road length:", error);
+    };
+
 };
