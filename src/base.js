551657c29afef5a215de8d31cb5f4c3a1aa90c13
qifan-wu <qifanw@umich.edu>
2024-03-15 05:07:15 -0400

    display metro lines, add metro stations and mapbox street basemap

--- src/base.js
+++ src/base.js
@@ -1,18 +1,70 @@
-import { STATIONS_DATA_FILE } from './constants.js'
+import { STATIONS_INFO_FILE, METRO_FILE } from './constants.js'
 import { createStationMarkers, handleSearchedPlace} from './utils.js';
-import { ARCGIS_ACCESS_TOKEN } from './private.js';
+import { ESRI_ACCESS_TOKEN as ESRI_ACCESS_TOKEN, MAPBOX_PUBLIC_TOKEN } from './private.js';
 
 var osmBaseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         });
 
-// map.addLayer(osmBaseMap);
 var satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri'});
 
+var mapboxStreet = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/about/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11', // Change this to your desired Mapbox style
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: MAPBOX_PUBLIC_TOKEN // Replace this with your Mapbox access token
+    })
+
+
+axios.get(METRO_FILE)
+    .then(function (response) {
+      var metroStations = L.markerClusterGroup();
+      var metroLines = L.markerClusterGroup();
+      var metroData = response.data;
+      for (const metroUrl in metroData) {
+          if (metroData.hasOwnProperty(metroUrl)) {
+            const metro = metroData[metroUrl];
+            // Plot metro lines
+            metro.lines.forEach(function(line) {
+              let lineCoordinates = line.path.map(function(coord) {
+                return [coord.lat, coord.lng];
+              });
+              var linePoly = L.polyline(lineCoordinates, { color: line.color });
+              metroLines.addLayer(linePoly);
+              metroLines.on('click', function(e) {
+                  this.bindPopup("line name: " + line.name +
+                                  "<br>line branch: " + line.branch).openPopup();
+              });
+            });
+
+            metro.stations.forEach(function(station) {
+              let stationMarkerNew = L.marker([station.lat, station.lon]);
+              metroStations.addLayer(stationMarkerNew);
+            });
+
+          }
+        }
+        // map.addLayer(metroStations);
+        map.addLayer(metroLines);
+
+        layerControl.addOverlay(metroLines, "metroLines");
+        overlayMaps.metroLines = metroLines;
+
+        layerControl.addOverlay(metroStations, "metroStations");
+        overlayMaps.metroStations = metroStations;
+    })
+    .catch(function (error) {
+        console.error("error fetching cache: ", error)
+    });
+
+
 var baseMaps = {
     "OpenStreetMap": osmBaseMap,
-    "Satellite": satellite
+    "Satellite": satellite,
+    'Streets': mapboxStreet
 };
 
 export var overlayMaps = {
@@ -43,11 +95,7 @@ const searchControl1 = L.esri.Geocoding.geosearch({
         useMapBounds: false,
         providers: [
           L.esri.Geocoding.arcgisOnlineProvider({
-            apikey: ARCGIS_ACCESS_TOKEN,
-            nearby: {
-              lat: -33.8688,
-              lng: 151.2093
-            }
+            apikey: ESRI_ACCESS_TOKEN
           })
         ]
       }).addTo(map);
@@ -60,18 +108,14 @@ searchControl1.on("results", function (data) {
         }
       });
 
-
-axios.get(STATIONS_DATA_FILE)
+axios.get(STATIONS_INFO_FILE)
     .then(function (response) {
         var stationsData = response.data;
-        console.log(new Date().toLocaleString());
         var stationMarkers = createStationMarkers(stationsData);
-        console.log(new Date().toLocaleString());
     })
     .catch(function (error) {
         console.error("error fetching cache: ", error)
     });
-
 
 
 
