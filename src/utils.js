17cd5cc87479d1d0f0e2f74523bca4a5ceaa2258
qifan-wu <qifanw@umich.edu>
2024-04-12 16:10:30 -0400

    text update

--- src/utils.js
+++ src/utils.js
@@ -266,7 +266,7 @@ export async function searchPOICat(lat, lon, category, detailCat) {
                 let link = `${OSM_ORG_ENDPOINT}/${info.type}/${info.osm_id}`;
                 let descrip = `<a href="#"
                     onclick="(function(url) { window.open(url, '_blank'); })('${link}'); return false;">
-                    See something wrong? Click here to update</a>`;
+                    See something wrong? Click here to update it with OSM editor</a>`;
                 return descrip;
             },
             body: function (info) {
